634d406117f1bf0f6a6ac8e279e005c91bfdff60
Roberto Zegers <rzegers@puc.cl>
2018-08-17 08:45:34 -0700

    Create symbols for DH parameters, Create Modified DH parameters (table), Create individual transformation matrices

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -17,6 +17,8 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint # used to 
 from geometry_msgs.msg import Pose # in test mode, we will receive an IK request from the simulator with end-effector poses
 from mpmath import * # or facilitating symbolic math in python
 from sympy import *
+import numpy as np
+from numpy import array
 
 
 def handle_calculate_IK(req):
@@ -29,17 +31,59 @@ def handle_calculate_IK(req):
     else:
 
         ### First part of implementation is to obtain individual transformation matrices from our calculated DH Parameters table:
-        # Create symbols
-	#
+        # Create symbols for joint variables
+        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8') # theta_i
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
 	#
 	# Create Modified DH parameters
-	#
+	# KUKA KR210
+        s = {alpha0: 0, a0: 0, d1: 0.75
+             alpha1: -pi/2, a1: 0.35, d2: 0, q2: q2-pi/2,
+             alpha2: 0, a2: 1.25, d3: 0,
+             alpha3: -pi/2, a3: -0.054, d4: 1.5,
+             alpha4: pi/2, a4: 0, d5: 0,
+             alpha5: -pi/2, a5: 0, d6: 0,
+             alpha6: 0, a6: 0, d7: 0.303, q7: 0}
 	#
 	# Define Modified DH Transformation matrix
-	#
-	#
 	# Create individual transformation matrices
-	#
+        T0_1 = Matrix([[            cos(q1),            -sin(q1),            0,              a0],
+                       [sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                       [sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                       [                  0,                   0,            0,               1]])
+
+        T1_2 = Matrix([[           cos(q2),            -sin(q2),            0,              a1],
+                      [sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                      [sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                      [                  0,                   0,            0,               1]])
+
+
+        T2_3 = Matrix([[           cos(q3),            -sin(q3),            0,              a2],
+                      [sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+                      [sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+                      [                  0,                   0,            0,               1]])
+
+        T3_4 = Matrix([[           cos(q4),            -sin(q4),            0,              a3],
+                      [sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+                      [sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+                      [                  0,                   0,            0,               1]])
+
+        T4_5 = Matrix([[           cos(q5),            -sin(q5),            0,              a4],
+                      [sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+                      [sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+                      [                  0,                   0,            0,               1]])
+
+        T5_6 = Matrix([[           cos(q6),            -sin(q6),            0,              a5],
+                      [sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+                      [sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+                      [                  0,                   0,            0,               1]])
+
+        T6_G = Matrix([[           cos(q7),            -sin(q7),            0,              a6],
+                      [sin(q7)*cos(alpha6), cos(q7)*cos(alpha6), -sin(alpha6), -sin(alpha6)*d7],
+                      [sin(q7)*sin(alpha6), cos(q7)*sin(alpha6),  cos(alpha6),  cos(alpha6)*d7],
+                      [                  0,                   0,            0,               1]])
 	#
 	# Extract rotation matrices from the transformation matrices
 	#
